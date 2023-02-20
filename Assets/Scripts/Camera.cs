dfca8af5dcd1fc15028aeb6dfdc32d9497dd8fe2
Jean-Pierre <jpphi@yahoo.fr>
2023-02-20 12:26:46 +0100

    La camera suis le joueur au niveau 3

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] GameObject _player;
+
+    Vector3 offset;
+    Quaternion rot;
+    void Start()
+    {
+        rot= Quaternion.Euler(90,0,0);
+        offset= new Vector3(0,20,0);
+        //rot= transform.rotation;
+        transform.position = offset;
+        transform.rotation = rot;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position= _player.transform.position + offset;
+        
+    }
+}
